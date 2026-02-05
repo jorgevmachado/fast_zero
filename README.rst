cf1564011d7e7cdf96e66aecff35d2edbda325de
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2026-02-05 16:42:28 -0300

    big bang

---
+++ README.rst
@@ -0,0 +1,5 @@
+Toda vez que abrir o projeto:
+## poetry env use 3.13
+## poetry env activate
+## source /home/jorge.machado/.cache/pypoetry/virtualenvs/fast-zero-dVL4rHQI-py3.13/bin/activate
+## fastapi dev fast_zero/app.py
---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_should_return_hello_world():
+    """
+    Esse teste tem 3 etapas (AAA)
+    - A: Arrange - Arranjo
+    - A: Act - Executa a coisa (o SUT)
+    - A: Assert - Garanta que A é A
+    """
+    # arrange
+    client = TestClient(app)
+
+    # ACT
+    response = client.get('/')
+
+    # Assert
+    assert response.json() == {'message': 'Hello World!'}
+    assert response.status_code == HTTPStatus.OK
